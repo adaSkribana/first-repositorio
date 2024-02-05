2a997ebe6623ade9682d4a95d2f92e97c441d22c
adaSkribana <136006242+adaSkribana@users.noreply.github.com>
2024-02-05 13:16:25 -0300

    versionfinal.py

--- ejercicioPractico_12.py
+++ ejercicioPractico_12.py
@@ -1,28 +1,11 @@
-""""numeros=[1, 2, 3, 4, 5]
-print(f"El contenido original de mi lista {numeros}")
-numeros_pop=[ ]
-numeros_pop.append(numeros.pop(0))
-numeros_pop.append(numeros.pop(-1))
-print(f"El contenido actual de mi lista {numeros} \n Lista numeros eliminados {numeros_pop} ")
-print("***************************************************************")
-print("***La ejemplificacion ha finalizado, reinicie para continuar***")
-print("***************************************************************")
-"""
 
-lista= [ 1, 2, 3, 4, 5]
-variable_help=[1, 5, 2, 4, 3 ]
-lista_pop= [ ]
-var_help2= [ ]
-print(f"El contenido inicial es el siguiente:\n lista original: {lista}\n lista de numeros eliminados por sentencia: {lista_pop} \n La ejucucion ha comenzado. ")
-while variable_help != lista_pop:
-    if lista == var_help2:
-        break
-lista_pop.append(lista.pop(0))
-lista_pop.append(lista.pop())
-   
-print(f"El contenido resultante es el siguiente:\n lista original modificada: {lista_pop}\n lista de numeros eliminados por el bucle: {lista_pop} \n La ejucucion ha finalizado ")
-
-
-
-
+var= int(input  ("ingresa digitos numericos: " ))
+lista_help=[]
+numeros=range(1,101)
+for i in  numeros:
+    if i != var: 
+        lista_help.append(i)
+    else:
+        pass
+print(lista_help)
 
